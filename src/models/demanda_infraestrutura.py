bc31a533c1be55514ccd5d534903a6558dfeb1cb
Henrique Coimbra <henrique.coimbra@aluno.ufca.edu.br>
2026-02-07 19:19:19 -0300

    Integração das classes de demanda

---
+++ src/models/demanda_infraestrutura.py
@@ -0,0 +1,26 @@
+from models.demanda import Demanda
+
+class DemandaInfraestrutura(Demanda):
+    """
+    Essa docstring tem a função de ordenar as demandas de estruturas, passando por um 
+    processo de solicitação que validam as informações, nível de importância e coleta os 
+    dados dos envolvidos. 
+    """
+    def __init__(self, id_demanda, descricao, prioridade, solicitante, custo_estimado, localizacao_demanda):
+        super().__init__(id_demanda, descricao, prioridade, solicitante)
+        self.__custo_estimado = custo_estimado
+        self.__localizacao_demanda = localizacao_demanda
+
+    def processar_solicitacao(self, usuario):
+        if self.id_municipio != usuario.id_municipio: 
+            print(f"Acesso negado! o usuário {usuario.nome} não pertence a esse município") 
+            return 
+        if self.__custo_estimado > 100_000: 
+            print(f"O custo estimado é maior do que o disponível, a obra entrará em processo de licitação")
+            self.atualizar_status ("EM LICITAÇÃO")
+        else: 
+            print(f"O custo para esta obra foi aprovado, entrando em execução")
+            self.atualizar_status("EM ANDAMENTO")
+
+        self.emitir_notificacao_critica()
+        self.atualizar(usuario)
